9e6146887076de4d00919dbdae631f6ad39634f1
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-10-25 23:32:30 -0300

    Ordered some files, started audio data preprocessing
    owowowowo 🤔🤔🤔🤔😉😉👍😉😉😉🤡🤡🤡🐸🐼🐸🐼🐸

---
+++ audio/audio_data.py
@@ -0,0 +1,137 @@
+"""
+Module to handle and preprocess audio dataset
+Based on: https://towardsdatascience.com/audio-deep-learning-made-simple-sound-classification-step-by-step-cebc936bbe5
+
+Made by Ammi Beltrán, Fernanda Borja & Luciano Vidal
+"""
+import math, random
+import torch
+import torchaudio
+from torchaudio import transforms
+from IPython.display import Audio
+from torch.utils.data import DataLoader, Dataset, random_split
+import torchaudio
+
+# Load an audio file. Return the signal as a tensor and the sample rate
+class AudioUtil():
+    @staticmethod
+    def open(audio_file):
+        sig, sr = torchaudio.load(audio_file)
+        return (sig, sr)
+
+
+    # Convert the given audio to the desired number of channels
+    @staticmethod
+    def rechannel(aud, new_channel):
+        sig, sr = aud
+
+        if (sig.shape[0] == new_channel):
+            # Nothing to do
+            return aud
+
+        if (new_channel == 1):
+            # Convert from stereo to mono by selecting only the first channel
+            resig = sig[:1, :]
+        else:
+            # Convert from mono to stereo by duplicating the first channel
+            resig = torch.cat([sig, sig])
+
+        return ((resig, sr))
+    
+     # Since Resample applies to a single channel, we resample one channel at a time
+    @staticmethod
+    def resample(aud, newsr):
+        sig, sr = aud
+
+        if (sr == newsr):
+        # Nothing to do
+            return aud
+
+        num_channels = sig.shape[0]
+        # Resample first channel
+        resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+        if (num_channels > 1):
+        # Resample the second channel and merge both channels
+            retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+            resig = torch.cat([resig, retwo])
+
+        return ((resig, newsr))
+    
+  # Pad (or truncate) the signal to a fixed length 'max_ms' in milliseconds
+    @staticmethod
+    def pad_trunc(aud, max_ms):
+        sig, sr = aud
+        num_rows, sig_len = sig.shape
+        max_len = sr//1000 * max_ms
+
+        if (sig_len > max_len):
+        # Truncate the signal to the given length
+            sig = sig[:,:max_len]
+
+        elif (sig_len < max_len):
+        # Length of padding to add at the beginning and end of the signal
+            pad_begin_len = random.randint(0, max_len - sig_len)
+            pad_end_len = max_len - sig_len - pad_begin_len
+
+            # Pad with 0s
+            pad_begin = torch.zeros((num_rows, pad_begin_len))
+            pad_end = torch.zeros((num_rows, pad_end_len))
+
+            sig = torch.cat((pad_begin, sig, pad_end), 1)
+        
+        return (sig, sr)
+    
+    @staticmethod
+    def spectro_gram(aud, n_mels=64, n_fft=1024, hop_len=None):
+        sig,sr = aud
+        top_db = 80
+
+        # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+        spec = transforms.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)(sig)
+
+        # Convert to decibels
+        spec = transforms.AmplitudeToDB(top_db=top_db)(spec)
+        return (spec)
+    
+
+
+
+"""
+Sound Dataset
+"""
+class SoundDS(Dataset):
+  def __init__(self, df, data_path):
+    self.df = df
+    self.data_path = str(data_path)
+    self.duration = 4000
+    self.sr = 44100
+    self.channel = 2
+    self.shift_pct = 0.4
+            
+  # Number of items in dataset
+  def __len__(self):
+    return len(self.df)    
+    
+  # Get i'th item in dataset
+  def __getitem__(self, idx):
+    # Absolute file path of the audio file - concatenate the audio directory with
+    # the relative path
+    audio_file = self.data_path + self.df.loc[idx, 'relative_path']
+    # Get the Class ID
+    class_id = self.df.loc[idx, 'classID']
+
+    aud = AudioUtil.open(audio_file)
+    # Some sounds have a higher sample rate, or fewer channels compared to the
+    # majority. So make all sounds have the same number of channels and same 
+    # sample rate. Unless the sample rate is the same, the pad_trunc will still
+    # result in arrays of different lengths, even though the sound duration is
+    # the same.
+    reaud = AudioUtil.resample(aud, self.sr)
+    rechan = AudioUtil.rechannel(reaud, self.channel)
+
+    dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
+    # shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
+    sgram = AudioUtil.spectro_gram(dur_aud, n_mels=64, n_fft=1024, hop_len=None)
+    # aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
+
+    return sgram, class_id
