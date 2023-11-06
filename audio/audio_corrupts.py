6082139b00b51c7023b58da76751f121425283a8
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-11-06 17:33:15 -0300

    Update audio_corrupts.py

--- audio/audio_corrupts.py
+++ audio/audio_corrupts.py
@@ -3,24 +3,33 @@ import torchaudio
 import torch
 
 def gaussian_audio(signal, std = 0.02):
-    size = signal.shape
-    noise = np.random.normal(loc=0.0, 
-                         scale=std, 
-                         size=size)
-    output = signal + noise
-    return output
+    #
+    sig, sr = signal
+    for i, side in enumerate(sig):
+        size = sig.shape
+        noise = np.random.normal(loc=0.0, 
+                            scale=std, 
+                            size=size)
+        output = sig + noise
+        sig[i] = output
+    return sig, sr
 
 
 def filter_audio(signal, Fs = 44100, central_freq=500, Q=10):
-    signal_torch = torch.from_numpy(signal)
-    output = torchaudio.functional.bandpass_biquad(signal_torch, 
-                                                   Fs,
-                                                   central_freq, 
-                                                   Q).numpy()
-    return output
+    #
+    sig, sr = signal
+    # signal_torch = torch.from_numpy(sig)
+    for i, side in enumerate(sig):
+        output = torchaudio.functional.bandpass_biquad(side, 
+                                                    Fs,
+                                                    central_freq, 
+                                                    Q).numpy()
+        sig[i] = output
+    return sig, sr
 
 
 def silent_audio(signal, window_num = 10, prob = 0.5):
+    #
     sig, sr = signal
     signal_window = sig.clone().reshape((2, window_num, -1))
     for wnd in signal_window:
@@ -35,15 +44,18 @@ def silent_audio(signal, window_num = 10, prob = 0.5):
 
 
 def random_audio_permute(signal):
-    window_len= int(len(signal)/4)
-    signal_slice = signal[:(4*window_len)].copy()
-    signal_slice = signal_slice.reshape((4, -1))
+    sig, sr = signal
+    for i, side in enumerate(sig):
+        window_len= int(len(side)/4)
+        signal_slice = signal[:(4*window_len)].copy()
+        signal_slice = signal_slice.reshape((4, -1))
 
-    order = np.linspace(0,3,4, dtype='int')
-    np.random.shuffle(order)
+        order = np.linspace(0,3,4, dtype='int')
+        np.random.shuffle(order)
 
-    signal_copy = np.zeros_like(signal_slice)
-    for i, idx in enumerate(order):
-        signal_copy[i] = signal_slice[idx]
-    output = signal_copy.reshape(len(signal))
-    return output
+        signal_copy = np.zeros_like(signal_slice)
+        for i, idx in enumerate(order):
+            signal_copy[i] = signal_slice[idx]
+        output = signal_copy.reshape(len(signal))
+        sig[i] = output
+    return sig, sr
