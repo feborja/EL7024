8f19788169daf3f7f659ab66806b0350de521e80
Fernanda Borja <102128933+feborja@users.noreply.github.com>
2023-11-06 17:27:56 -0300

    Update img_corrupts.py

--- img_corrupts.py
+++ img_corrupts.py
@@ -9,7 +9,7 @@ Corruption functions for image based data
 Made by Ammi Beltrán, Fernanda Borja & Luciano Vidal
 '''
 # Label Related
-def random_labels(train_data, prob):
+def random_labels(train_data, prob = 0.4):
     # data, labels = train_data
     # unique, count = torch.unique(labels)
     for i in range(len(train_data)):
@@ -26,44 +26,50 @@ def random_labels(train_data, prob):
 '''
 Use as transform.Lambda(function)
 '''
-def pixel_permute(img):
-    # make a flat image
-    img = img.view(3, -1)
-    # Get permutation for all
-    idx = torch.randperm(img.shape[1], generator=torch.manual_seed(69))
-    print(idx[6])
-    for i, ch in enumerate(img):
-        ch = ch[idx].view(ch.size())
-        img[i] = ch
-    img = torch.reshape(img, (3, 32, 32))
-    print(img.shape)
+def pixel_permute(img, prob = 0.4):
+    luck = torch.rand(1)
+    if (luck < prob):
+        # make a flat image
+        img = img.view(3, -1)
+        # Get permutation for all
+        idx = torch.randperm(img.shape[1], generator=torch.manual_seed(69))
+        print(idx[6])
+        for i, ch in enumerate(img):
+            ch = ch[idx].view(ch.size())
+            img[i] = ch
+        img = torch.reshape(img, (3, 32, 32))
+        print(img.shape)
     return (img)
 
-def random_pixel_permute(img):
-    # make a flat image
-    img = img.view(3, -1)
-    # Get permutation for all
-    idx = torch.randperm(img.shape[1])
-    print(idx[6])
-    for i, ch in enumerate(img):
-        ch = ch[idx].view(ch.size())
-        img[i] = ch
-    img = torch.reshape(img, (3, 32, 32))
-    print(img.shape)
+def random_pixel_permute(img, prob = 0.4):
+    luck = torch.rand(1)
+    if (luck < prob):
+        # make a flat image
+        img = img.view(3, -1)
+        # Get permutation for all
+        idx = torch.randperm(img.shape[1])
+        print(idx[6])
+        for i, ch in enumerate(img):
+            ch = ch[idx].view(ch.size())
+            img[i] = ch
+        img = torch.reshape(img, (3, 32, 32))
+        print(img.shape)
     return (img)
 
-def gaussian_pixels(img):
-    # make a flat image
-    img = img.view(3, -1)
-    stats = torch.empty([3, 2]) # mean, var
-    # print(stats, stats.shape)
-    for i, ch in enumerate(img):
-        stats[i][0] = torch.mean(ch, axis = 0)
-        stats[i][1] = torch.var(ch, axis = 0)
-        x = np.random.default_rng(seed=69).normal(loc=stats[i][0].item(), scale=stats[i][1].item(), size=img.shape[1])
-        # x = np.random.normal(loc=stats[i][0].item(), scale=stats[i][1].item(), size=img.shape[1])
-        img[i] = torch.from_numpy(x)
-    img = torch.reshape(img, (3, 32, 32))
+def gaussian_pixels(img, prob = 0.4):
+    luck = torch.rand(1)
+    if (luck < prob):
+        # make a flat image
+        img = img.view(3, -1)
+        stats = torch.empty([3, 2]) # mean, var
+        # print(stats, stats.shape)
+        for i, ch in enumerate(img):
+            stats[i][0] = torch.mean(ch, axis = 0)
+            stats[i][1] = torch.var(ch, axis = 0)
+            x = np.random.default_rng(seed=69).normal(loc=stats[i][0].item(), scale=stats[i][1].item(), size=img.shape[1])
+            # x = np.random.normal(loc=stats[i][0].item(), scale=stats[i][1].item(), size=img.shape[1])
+            img[i] = torch.from_numpy(x)
+        img = torch.reshape(img, (3, 32, 32))
     return img
 
 '''
